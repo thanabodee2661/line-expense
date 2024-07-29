1d50de648e9dee45eacf4941160f6b237218db7a
thanabodee <thanabodee2661@gmail.com>
2024-07-29 18:52:21 +0700

    disabled useEffect

--- src/app/clientLayout.tsx
+++ src/app/clientLayout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/header";
 import { useEffect, useState } from "react";
 import liff, { Liff } from "@line/liff";
 import LiffContext from "@/contexts/line";
+import { Profile } from "@liff/get-profile";
 
 function ClientLayout({
   children,
@@ -12,8 +13,9 @@ function ClientLayout({
 }>) {
   const [liffObject, setLiffObject] = useState<Liff | null>(null);
   const [liffError, setLiffError] = useState<string | null>(null);
+  const [profile, setProfile] = useState<Profile>();
 
-  useEffect(() => {
+  // useEffect(() => {
     liff
       .init({ liffId: "2005736477-2eRaENlj" })
       .then(() => {
@@ -21,12 +23,17 @@ function ClientLayout({
           liff.login();
         }
         setLiffObject(liff);
+
+        liffObject?.getProfile()
+        .then((pf) => {
+          setProfile(pf);
+        });
       })
       .catch((error) => {
         console.log(`liff.init() failed: ${error}`);
         setLiffError(error.toString());
       });
-  }, []);
+  // }, []);
 
   return (
     <LiffContext.Provider value={liffObject}>
